1b60eef60e80814550a617195302d02a255cae93
paulie <tempo.pause@gmail.com>
2015-10-28 15:38:05 +0100

    Testing anaconda ide in sublime

--- app/models.py
+++ app/models.py
@@ -3,35 +3,38 @@
 
 """ Database models """
 
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask_table import Table, Col  # , create_table
+from flask import url_for
+from sqlalchemy import inspect
+from wtforms.validators import Email, Length
+from wtforms import PasswordField
+
 #############################################
 # DB INIT
-from flask.ext.sqlalchemy import SQLAlchemy
 # no app object passed! Instead we use use db.init_app in the factory.
 db = SQLAlchemy()
 
+
 #############################################
 # Convert an SQLALCHEMY model into a Flask table
-
-from flask_table import Table, Col#, create_table
-from flask import url_for
-from sqlalchemy import inspect
-
 class ItemTable(Table):
+
     def thead(self):
-        return '<thead class="thead-default"><tr>{}</tr></thead>'\
-            .format(''.join( (self.th(col_key, col) \
-            for col_key, col in self._cols.items() if col.show)))
+        return '<thead class="thead-default"><tr>{}</tr></thead>' \
+            .format(''.join((self.th(col_key, col)
+                    for col_key, col in self._cols.items() if col.show)))
 
     def sort_url(self, col_key, reverse=False):
         if reverse:
-            direction =  'desc'
+            direction = 'desc'
         else:
             direction = 'asc'
-# // TO FIX:
+# // TO FIX:
 # do not like the 'view' link here!
         return url_for('.view', sort=col_key, direction=direction)
 
-# Note: bootstrap can apply label colors to row
+# Note: bootstrap can apply label colors to row
     # def tr_format(self, item):
     #     print(item)
     #     if item.important():
@@ -39,48 +42,50 @@ class ItemTable(Table):
     #     else:
     #         return '<tr>{}</tr>'
 
+
 def create_table(name):
     return type(name, (ItemTable,), {})
+
 
 def model2table(obj):
     """ Give me an SQLALCHEMY obj to get an HTML table """
 
     table_name = 'Table' + obj.__name__
     TableCls = create_table(table_name)
-    #print("Table:", table_name)
+    # print("Table:", table_name)
 
     mapper = inspect(obj)
     for column in mapper.attrs:
         colname = column.key.replace('_', ' ').capitalize()
-        #print("SQLALCHEMY col", colname)
+        # print("SQLALCHEMY col", colname)
         TableCls.add_column(column.key, Col(colname))
 
-    TableCls.classes = ['table', 'table-hover'] #,'table-striped']
+    TableCls.classes = ['table', 'table-hover']
     TableCls.allow_sort = True
 
     return TableCls
 
+
 #############################################
 # Work on models
-
-from wtforms.validators import Email, Length
-from wtforms import PasswordField
-
 class MyModel(db.Model):
-    ## Primary key
+
+    # Primary key
     id = db.Column(db.BigInteger, autoincrement=True, primary_key=True)
-    ## Normal
-    name = db.Column(db.Unicode(5), nullable=False, \
-        info={'validators': Length(min=5,max=255)})
-    ## Custom validator
-    email = db.Column(db.Unicode(255), nullable=False, info={'validators': Email()})
-    ## Test SELECT
-    # enum sqlalchemy tutorial http://techspot.zzzeek.org/2011/01/14/the-enum-recipe/
-    test_select_a = db.Column(db.Enum('part_time', 'full_time', 'contractor', name='employee_types'))
-    # test_select_b = db.Column(db.Integer, \
-    #     info={'choices': [(i, i) for i in range(13, 99)]}, nullable=False)
-    ## Password field from WTForm types
-    password = db.Column(db.String(255), info={'form_field_class': PasswordField} )
+    # Normal
+    name = db.Column(db.Unicode(5), nullable=False,
+                     info={'validators': Length(min=5, max=255)})
+    # Custom validator
+    email = db.Column(db.Unicode(255), nullable=False,
+                      info={'validators': Email()})
+    # Test SELECT
+    # enum sqlalchemy tutorial
+    # http://techspot.zzzeek.org/2011/01/14/the-enum-recipe/
+    test_select_a = db.Column(db.Enum(
+        'part_time', 'full_time', 'contractor', name='employee_types'))
+    # Password field from WTForm types
+    password = db.Column(db.String(255),
+                         info={'form_field_class': PasswordField})
 
 MyTable = model2table(MyModel)
 
