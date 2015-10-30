424cec2ad3d09c06778625e8d1fb8fbb92d1f1ba
paulie <tempo.pause@gmail.com>
2015-10-30 15:29:21 +0100

    Quick fixes

--- app/__init__.py
+++ app/__init__.py
@@ -96,7 +96,8 @@ def create_app(config_filename):
     # Logging
     @app.after_request
     def log_response(resp):
-        app.logger.info("{} {} {}\n{}".format(req.method,req.url,req.data,resp))
+        app.logger.info("{} {} {}\n{}".format(
+            req.method,req.url,req.data,resp))
         return resp
 
     return app
